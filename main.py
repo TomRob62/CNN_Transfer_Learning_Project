826f2ee25050498ccf470cd51cb468d7bfc99bd9
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-03-28 21:25:35 -0400

    Doc main.py
    Adding program description and author information

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Thomas Roberts
+CS 4732
+Professor Karakaya
+March 28, 2024
+
+This program will try to implement a CNN with GoogLeNet, VGG, and ResNet50 and
+compare the results
+"""
+
+import tensorflow
