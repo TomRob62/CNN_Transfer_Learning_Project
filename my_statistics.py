de30b9cf3b146ed32048b333a21d211deeba0e3a
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-03-31 21:31:58 -0400

    chore updating statistics
    adding variable momentum to statistics class

--- my_statistics.py
+++ my_statistics.py
@@ -1,6 +1,8 @@
 class My_Statistics:
     epoch = 0
     lr = 4
+    name = ""
+    momentum = 0
 
     training_acc = 0
     valid_acc = 0
@@ -9,14 +11,16 @@ class My_Statistics:
     training_loss = 0
     valid_loss = 0
     test_loss = 0
-    def __init__(self, epoch, learning_rate):
+    def __init__(self, epoch, learning_rate, name, momentum):
         self.epoch = epoch
         self.lr = learning_rate
+        self.name = name
+        self.momentum = momentum
         return None
 
     def print_statistics(self) ->  None:
         """Prints statistics to console"""
-        print("Statistics for Model with %s epoch and learning rate %s." % (self.epoch, self.lr))
+        print("Statistics for %s Model with %s epoch, %s learning rate, %s momentum." % (self.name, self.epoch, self.lr, self.momentum))
         print("\nTraining Loss: %s\t\tTraining Accuracy: %s" % (self.training_loss, self.training_acc))
         print("\nValidation Loss: %s\t\tValidation Accuracy: %s" % (self.valid_loss, self.valid_acc))
         print("\nTesting Loss: %s\t\tTesting Accuracy: %s\n\n" % (self.test_loss, self.test_acc))
@@ -25,7 +29,7 @@ class My_Statistics:
     def save_statistics(self) -> None:
         """Saves statistics to text file "Statistics.txt"""
         file = open("Statistics.txt", "a")
-        file.write("Statistics for Model with %s epoch and learning rate %s." % (self.epoch, self.lr))
+        file.write("Statistics for %s Model with %s epoch, %s learning rate, %s momentum." % (self.name, self.epoch, self.lr, self.momentum))
         file.write("\nTraining Loss: %s\t\tTraining Accuracy: %s" % (self.training_loss, self.training_acc))
         file.write("\nValidation Loss: %s\t\tValidation Accuracy: %s" % (self.valid_loss, self.valid_acc))
         file.write("\nTesting Loss: %s\t\tTesting Accuracy: %s\n\n" % (self.test_loss, self.test_acc))
