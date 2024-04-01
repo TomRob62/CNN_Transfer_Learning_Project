19af90b7abc26a7ad8ac59ae72131a1dd44a41b7
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-03-31 21:36:31 -0400

    docs updating main.py program description

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ CS 4732
 Professor Karakaya
 March 28, 2024
 
-This program will try to implement a CNN with GoogLeNet, VGG, and ResNet50 and
+This program will try to implement a CNN with VGG and ResNet50 then
 compare the results
 """
 
