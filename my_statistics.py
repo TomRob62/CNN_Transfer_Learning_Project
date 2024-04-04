cae84f3deb81bc58d85c46017cedea21ba1456dd
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-04-03 20:58:46 -0400

    feat adding statistics manager class
    adding statistics manager class to program and adding method get_average accuracy to statistics object class

--- my_statistics.py
+++ my_statistics.py
@@ -8,6 +8,8 @@
     deep learning models. It records optimizer information and results from training.
 
 """
+
+
 class Statistics_Object:
     """
     Description: Class that holds the information/statistics for a single model
@@ -87,8 +89,8 @@ class Statistics_Object:
         self.accuracy.append(obj)
         return None
     # end definition add_accuracy()
-    
-    def add_loss(self, loss: float, name:str = "None") -> None:
+
+    def add_loss(self, loss: float, name: str = "None") -> None:
         """
             Creates a list object with the name and loss. E.g. ["None", 0.510351]
             and appends it to the class attribute 'loss'
@@ -103,7 +105,7 @@ class Statistics_Object:
         self.loss.append(obj)
         return None
     # end definition add_loss()
-    
+
     def add_pair(self, pair: list, name: str = "None") -> None:
         """
             Creates two list objects with name and value (from pair). E.g. ["None", 0.510351]
@@ -123,17 +125,115 @@ class Statistics_Object:
         return None
     # end definition add_pair()
 
+    def get_average_accuracy(self) -> float:
+        """
+        Returns the average accuracy among the accuracies listed
+        """
+        avg_acc = 0
+        for acc in self.accuracy:
+            avg_acc = avg_acc + acc[1]
+        return avg_acc/len(self.accuracy)
+    # end of definition get_average_accuracy()
+
     def __str__(self) -> str:
         """
         Function to print the string representation of the object
         """
-        obj_string = "Model: %s" % self.name 
-        obj_string = obj_string + "\nOptimizer stats: name = %s, epoch = %s, batch_size = %s, \
-                                    learning_rate = %s, momentum = %s" % (self.optimizer_name, 
-                                                                          self.max_epoch, 
-                                                                          self.batch_size, 
-                                                                          self.learning_rate,
-                                                                            self.momentum)
-        obj_string = obj_string + "\n" + str(self.accuracy) + "\n" + str(self.loss)
+        obj_string = "\nModel: %s" % self.model_name
+        obj_string = obj_string + "\nOptimizer stats: name = %s, epoch = %s, batch_size = %s, learning_rate = %s, momentum = %s" % \
+            (self.optimizer_name,
+             self.max_epoch,
+             self.batch_size,
+             self.learning_rate,
+             self.momentum)
+        obj_string = obj_string + "\nAccuracy: " + \
+            str(self.accuracy) + "\nLoss: " + str(self.loss)
         return obj_string
 # end class Statistics
+
+
+class Statistics_Manager:
+    """
+    Description: This class provides functions for managing statistic objects
+
+    Attributes
+    ----------
+    objects_list
+
+    Functions
+    ----------
+    """
+    objects_list = ()
+
+    def __init__(self) -> None:
+        """
+        Constructor function
+        """
+        self.objects_list = ()
+        return None
+    # end of __init__()
+
+    def add(self, object: Statistics_Object) -> None:
+        """
+        appends the statistics object to class list
+        """
+        new_list = list(self.objects_list)
+        new_list.append(object)
+        self.objects_list = tuple(new_list)
+        return None
+    # end of definition add()
+    
+    def print_by_accuracy(self) -> None:
+        """
+        prints all statistics objects in order by accuracy
+        """
+        sorted_list = []
+        for current_obj in self.objects_list:
+            if len(sorted_list) == 0: # check if there objects in list
+                sorted_list.append(current_obj)
+            else: # 
+                index = 0
+                while index < len(sorted_list): # inserting object in miidle of list
+                    if current_obj.get_average_accuracy() > sorted_list[index].get_average_accuracy():
+                        sorted_list.insert(index, current_obj)
+                        break
+                    else:
+                        index = index +1
+                # end of while
+                if index == len(sorted_list): # if not inserted, appending to end of list
+                    sorted_list.append(current_obj)
+            # end of else
+        # end of for
+        for obj in sorted_list:
+            print(obj)
+        return None
+
+
+    def print_by_attribute(self, attribute_name) -> None:
+        """
+        Prints all statistics object grouped by an attribute
+        """
+        unsorted_list = list(self.objects_list)
+        sorted_list = []
+        while len(unsorted_list) > 0:
+            current_obj = unsorted_list[0]
+            for compare_obj in unsorted_list:
+                if getattr(compare_obj, attribute_name) == getattr(current_obj, attribute_name):
+                    sorted_list.append(compare_obj)
+                    unsorted_list.remove(compare_obj)
+            # end for
+        # end while
+        for obj in sorted_list:
+            print(obj)
+        return None
+    # end definition print_by_attribute
+
+    def print(self) -> None:
+        """
+        Prints all statistics objects in list
+        """
+        for obj in self.objects_list:
+            print(obj)
+
+        return None
+    # end of definition print
