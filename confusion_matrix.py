e593569c40a63196f0fc95cc8736f9f2e60f4e99
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-04-05 17:00:23 -0400

    creating confusion_matrix.py

---
+++ confusion_matrix.py
@@ -0,0 +1,10 @@
+"""
+Thomas Roberts
+CS 4732
+Professor Karakaya
+March 28, 2024
+
+Program Description: This program will generate the learning model with the best
+accuracy based on the results from main.py, and then generate a confusion
+matrix from that learning model.
+"""
